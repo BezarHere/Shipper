4607b830fee20801127cfab6f22c232a3f08a352
botatodev <botatodev@gmail.com>
2024-04-26 02:02:53 +0300

    Add project files.

---
+++ src/Argument.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipper;
+struct Argument
+{
+	public Argument(string name)
+	{
+		Name = name;
+		Uses = [];
+	}
+
+	private enum ReadingMode
+	{
+		Normal,
+		DoubleQuotes,
+		SingleQuota
+	}
+
+	private static Range ReadCommandlet(string source, int index)
+	{
+		int start = -1;
+		ReadingMode mode = 0;
+		for (int i = index; i < source.Length; i++)
+		{
+			if (start > -1)
+			{
+				if (mode == ReadingMode.Normal && char.IsWhiteSpace(source[i]))
+				{
+					return new(start, i);
+				}
+
+				if (mode == ReadingMode.DoubleQuotes && source[i] == '"')
+				{
+					
+				}
+
+
+
+				continue;
+			}
+
+			if (!char.IsWhiteSpace(source[i]))
+			{
+				if (source[i] == '"' || source[i] == '\'')
+				{
+					reading = source[i];
+				}
+				else
+				{
+					reading = 1;
+				}
+			}
+		}
+		return range;
+	}
+
+	private static Range[] SplitCommandlets()
+	{
+		return [];
+	}
+
+	public static Argument[] ParseArguments(string args)
+	{
+
+
+		return [];
+	}
+
+	public readonly string Name;
+	public List<string> Uses;
+
+	public readonly bool HasBeenRead { get => Uses.Count > 0; }
+
+}
+
+
---
+++ src/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Shipper;
+
+internal class Program
+{
+	public static int RunInteractive()
+	{
+		Console.WriteLine("Starting the interactive terminal:");
+
+		RunningInteractive = true;
+		int result = 0;
+
+		while (RunningInteractive)
+		{
+			Console.Write(">> ");
+			string line = (Console.ReadLine() ?? "quit").Trim();
+			if (line == "quit" || line == "exit" || line == "q")
+			{
+				RunningInteractive = false;
+				break;
+			}
+		}
+		return result;
+	}
+
+	private static int Run(Argument[] arguments)
+	{
+
+		return 0;
+	}
+
+	public static int Main(string[] args)
+	{
+		if (args.Length == 0)
+			return RunInteractive();
+
+		Argument[] arguments = new Argument[args.Length];
+		for (int i = 0; i < args.Length; i++)
+		{
+			arguments[i] = new(args[i]);
+		}
+
+		return Run(arguments);
+	}
+
+	public static Environment Environment { get; private set; } = new();
+	public static bool RunningInteractive { get; private set; } = false;
+}
