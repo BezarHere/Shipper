1706c9503bbbb3088d326ff7d40c9e0c560ccf03
botatodev <botatodev@gmail.com>
2024-04-28 18:04:16 +0300

    add ParseError exception

---
+++ src/ParseError.cs
@@ -0,0 +1,7 @@
+﻿namespace Shipper;
+
+internal class ParseError(string? message = null, IndexRange? span = null, string? source = null) : InvalidOperationException(message)
+{
+	public readonly IndexRange Span = span ?? new();
+	public readonly string? SourceCode = source;
+}
