63830076d14f8a7b2fc07ad83bbf233df81ee296
botatodev <botatodev@gmail.com>
2024-05-01 15:58:35 +0300

    starting to overhaul build script

---
+++ src/Script/Tokenizer.cs
@@ -0,0 +1,172 @@
+﻿using Shipper.TUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipper.Script;
+
+enum TokenType
+{
+	Unknown,
+	WhiteSpace,
+	NewLine,
+
+	String,
+	LiteralString,
+
+	EqualSign,
+	Comma,
+}
+
+record struct Token(TokenType Type, IndexRange Range, int Index = -1)
+{
+}
+
+internal static class Tokenizer
+{
+
+	public static IEnumerable<Token> Run(string source)
+	{
+		if (string.IsNullOrEmpty(source))
+			throw new ArgumentException($"'{nameof(source)}' cannot be null or empty.");
+
+		for (int i = 0; i < source.Length; i++)
+		{
+			if (source[i] == '\n')
+			{
+				int count = source.AsSpan(i).CountContinues(c => c == '\n');
+				yield return new Token(TokenType.NewLine, i..(i + count));
+				i += count - 1;
+				continue;
+			}
+
+			if (char.IsWhiteSpace(source[i]))
+			{
+				int count = source.AsSpan(i).CountContinues(c => c != '\n' && char.IsWhiteSpace(c));
+				yield return new Token(TokenType.WhiteSpace, i..(i + count));
+				i += count - 1;
+				continue;
+			}
+
+			if (source[i] == '=')
+			{
+				yield return new Token(TokenType.EqualSign, i..(i + 1));
+				continue;
+			}
+
+			if (source[i] == ',')
+			{
+				yield return new Token(TokenType.Comma, i..(i + 1));
+				continue;
+			}
+
+			if (source[i] == '\'' || source[i] == '"')
+			{
+				Token tk = ReadString(source, i);
+				// something fucked, the string might be unclosed
+				if (tk.Range.End > source.Length)
+				{
+					Highlight highlight = new()
+					{
+						Message = "Unclosed string",
+						Span = i..(i + 1),
+						Text = source
+					};
+					highlight.Draw();
+				}
+
+				i = tk.Range.End - 1;
+				yield return tk;
+				continue;
+			}
+
+			static bool func(char c)
+			{
+				if (char.IsWhiteSpace(c))
+					return false;
+				if (c == '=' || c == ',' || c == '\'' || c == '"')
+					return false;
+				return true;
+			}
+
+			// if non of the above condition is met, then this must be a regular name (string)
+			int char_count = source.AsSpan().CountContinues(func, i);
+
+			if (char_count == 0)
+			{
+				yield return new(TokenType.Unknown, i..(i + 1));
+			}
+
+			yield return new(TokenType.String, i..(i + char_count));
+			i += char_count - 1;
+		}
+
+		yield break;
+	}
+
+
+	private enum StringType
+	{
+		Normal,
+		Literal,
+		Multiline,
+		LiteralMultiline,
+	}
+
+	private static StringType GetStringType(string source, int start)
+	{
+		int len = source.Length - start;
+
+		// not enough chars
+		if (len == 0)
+			return StringType.Normal;
+
+		StringType type = source[start] == '"' ? StringType.Normal : StringType.Literal;
+		if (len < 6)
+			return type;
+
+		if (source[start] == source[start + 1] && source[start + 1] == source[start + 2])
+			return type == StringType.Normal ? StringType.Multiline : StringType.LiteralMultiline;
+		return type;
+	}
+
+	private static Token ReadString(string source, int start)
+	{
+		StringType type = GetStringType(source, start);
+		int type_len = type == StringType.Normal || type == StringType.Literal ? 1 : 3;
+		char type_char = source[start];
+		int original_start = start;
+
+		start += type_len;
+
+		int end = source.Length - type_len + 1;
+		for (int i = start; i < end; i++)
+		{
+			// escape (in a non-literal string)
+			if (source[i] == '\\' && (type == StringType.Normal || type == StringType.Multiline))
+			{
+				i++;
+				continue;
+			}
+
+			if (source[i] == type_char)
+			{
+				// found all the closing quotes
+				if (source.AsSpan().CountContinues(c => c == type_char, i) >= type_len)
+				{
+					// range includes the quotes
+					return new(type_char == '"' ? TokenType.String : TokenType.LiteralString, original_start..(i + type_len));
+				}
+			}
+
+		}
+
+		// the string was not closed!
+		return new(type_char == '"' ? TokenType.String : TokenType.LiteralString, original_start..int.MaxValue);
+	}
+
+
+}
---
+++ src/Script/Value.cs
@@ -0,0 +1,119 @@
+﻿namespace Shipper.Script;
+
+enum ValueType
+{
+	String,
+	Array,
+	Table,
+}
+
+internal struct Value : IEquatable<Value>
+{
+	private object _data = "";
+	public readonly ValueType Type { get; init; }
+
+	/// <summary>
+	/// creates a new value of type <paramref name="type"/> and sets it's data to the default value
+	/// </summary>
+	/// <param name="type">the value type</param>
+	public Value(ValueType type = ValueType.String)
+	{
+		Type = type;
+		if (type == ValueType.String)
+		{
+			_data = string.Empty;
+		}
+		else if (type == ValueType.Array)
+		{
+			_data = System.Array.Empty<Value>();
+		}
+		else
+		{
+			_data = new Dictionary<string, Value>();
+		}
+
+	}
+
+	public Value(string value)
+	{
+		Type = ValueType.String;
+		_data = value;
+	}
+
+	public Value(Value[] array)
+	{
+		Type = ValueType.Array;
+		_data = array;
+	}
+
+	public Value(Dictionary<string, Value> table)
+	{
+		Type = ValueType.Table;
+		_data = table;
+	}
+
+
+	public string String
+	{
+		readonly get
+		{
+			if (Type != ValueType.String)
+				throw new InvalidOperationException("Value isn't a string type");
+			return (string)_data;
+		}
+		set
+		{
+			if (Type != ValueType.String)
+				throw new InvalidOperationException("Value isn't a string type");
+			_data = value;
+		}
+	}
+
+	public Value[] Array
+	{
+		readonly get
+		{
+			if (Type != ValueType.Array)
+				throw new InvalidOperationException("Value isn't an array type");
+			return (Value[])_data;
+		}
+		set
+		{
+			if (Type != ValueType.Array)
+				throw new InvalidOperationException("Value isn't an array type");
+			_data = value;
+		}
+	}
+
+	public Dictionary<string, Value> Table
+	{
+		readonly get
+		{
+			if (Type != ValueType.Table)
+				throw new InvalidOperationException("Value isn't a table type");
+			return (Dictionary<string, Value>)_data;
+		}
+		set
+		{
+			if (Type != ValueType.Table)
+				throw new InvalidOperationException("Value isn't a table type");
+			_data = value;
+		}
+	}
+
+
+	public readonly bool Equals(Value other)
+	{
+		return Type == other.Type && _data == other._data;
+	}
+
+	public override readonly bool Equals(object? obj)
+	{
+		return obj is Value value && Equals(value);
+	}
+
+	public override readonly int GetHashCode()
+	{
+		return _data.GetHashCode();
+	}
+}
