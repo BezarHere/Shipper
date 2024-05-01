19792fe94234085711aff2f23b4d36cc7901f862
botatodev <botatodev@gmail.com>
2024-05-01 20:48:32 +0300

    ShipScript work to reimplement with the new tokenizer

--- src/Script/ShipScript.cs
+++ src/Script/ShipScript.cs
@@ -1,18 +1,31 @@
-﻿using Shipper.TUI;
-using System.Collections.Specialized;
-using System.Linq.Expressions;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
+﻿using System.Text;
 
 namespace Shipper.Script;
 
 static class ShipScript
 {
 
-	public static IEnumerable<KeyValuePair<string, Value>> Load(TextReader reader)
+	public static IEnumerable<KeyValuePair<string, Value>> Load(Stream reader)
 	{
-		
+		long len = reader.Length - reader.Position;
+		Span<byte> bytes = new byte[len];
+		reader.Read(bytes);
+		string text = UTF8Encoding.UTF8.GetString(bytes);
+		return Load(text);
 	}
+
+	public static IEnumerable<KeyValuePair<string, Value>> Load(string source)
+	{
+		foreach (var tk in Tokenizer.Run(source))
+		{
+			Console.WriteLine($"token: {tk.Type}, {tk.Range}, '{source[tk.Range.Start..tk.Range.End]}'");
+		}
+
+		Token[] tokens = Tokenizer.Run(source).ToArray();
+
+		yield break;
+	}
+
 	public static void Dump(TextWriter writer, KeyValuePair<string, Value> values)
 	{
 	}
