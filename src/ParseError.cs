1a91ad5b89757c543f988288578a673d187f15f1
botatodev <botatodev@gmail.com>
2024-05-02 21:09:56 +0300

    removed the old parse error

--- src/ParseError.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shipper;
-
-internal class ParseError(string? message = null, IndexRange? span = null, string? source = null) : InvalidOperationException(message)
-{
-	public readonly IndexRange Span = span ?? new();
-	public readonly string? SourceCode = source;
-}
