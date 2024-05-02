045d625d309e77cb9fe8b6291ad1289d5e95c984
botatodev <botatodev@gmail.com>
2024-05-02 21:09:41 +0300

    added the exceptions sub-namespace

---
+++ src/Exceptions/GlobParseError.cs
@@ -0,0 +1,7 @@
+﻿namespace Shipper.Exceptions;
+
+internal class GlobParseError(string message, IndexRange range, string line) : Exception(message)
+{
+	public readonly IndexRange Range = range;
+	public readonly string Line = line;
+}
---
+++ src/Exceptions/ShipParsingException.cs
@@ -0,0 +1,7 @@
+﻿namespace Shipper.Exceptions;
+
+internal class ShipParsingException(string message, long line_no = -1, long column = -1) : Exception(message)
+{
+	public readonly long LineNumber = line_no;
+	public readonly long Column = column;
+}
